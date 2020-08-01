e2f9b157d0d73532f0e5f3708bf89c8675fc5efb
月光虹 <iris@random.town>
2020-08-01 12:54:51 +0200

    Provide access to discoveries and suggestions from Peer to Peer Session

--- Sources/PeerDiscovery/PeerToPeerSession.swift
+++ Sources/PeerDiscovery/PeerToPeerSession.swift
@@ -1,8 +1,23 @@
+import Combine
+
 /// ...
+@available(OSX 10.15, *)
 public class PeerToPeerSession {
     
     /// The peer being announced and represented on the distributed network.
     public let impersonating: Peer
+
+    /// Reporting presence of the peers happens via the discoveries.
+    public let discoveries = PassthroughSubject<Peer, Never>() // TODO: Adjust failure.
+                             .share()
+                             .makeConnectable()
+    
+    /// Session may accept suggestions about peers.
+    ///
+    /// Accepting suggestions allows reliability feedback loop with subscribers of the `discoveries`.
+    ///
+    // TODO: Elaborate, or explain better! ^
+    public let suggestions = PassthroughSubject<Peer, Never>() // TODO: Adjust failure type too.
     
     /// Create a peer-to-peer connectivity session.
     public init(as peer: Peer) {
